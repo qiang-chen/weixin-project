1163dbfd1947c4c25891541016b884f8eeb23534
qiang-chen <1715584439@qq.com>
2019-07-17 20:58:59 +0800

    楼层效果的实现

---
+++ components/list/list.js
@@ -0,0 +1,81 @@
+// components/list/list.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  lifetimes: {
+    ready: function () {
+      let arr = [];
+
+      //console.log("ready",this.data.contentList)
+      //获取所有的标题 距离顶部的距离 把其存放到一个数组中去
+      this.createSelectorQuery().selectAll(".content").boundingClientRect((rect) => {
+        //加了定时器就能获取到
+        //卧槽 太激动了 解决了  这个破周期有bug只会执行一次  然后数据初次请求过来的
+        //是空数组 走了这个周期以后下次有了数据就不会再走了 日了狗了 
+        //解决方案  在父组件中传这个数组的时候判断一下是不是空数组
+        //console.log(rect, "---")
+        rect.forEach(item => {
+          arr.push(item.top)
+        })
+
+        //将arr赋值到全局data上面
+        this.setData({
+          saveTitle: arr
+        }, () => {
+          //console.log(this.data.saveTitle)
+        })
+
+      }).exec((res) => {
+      })
+
+    },
+    attached: function () {
+      // 在组件实例进入页面节点树时执行
+
+      //在这里打印data的时候  properties也属于data的变量
+      //console.log("attached", this.data.saveTitle)
+
+
+
+
+
+    },
+    detached: function () {
+      // 在组件实例被从页面节点树移除时执行
+
+    }
+  },
+  properties: {
+    contentList: Array,
+    currentKey: String
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    //定义组件自己的变量
+    saveTitle: []
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleScroll(e) {
+      let event = e;
+      //这个函数只要页面发生滑动就会触发  是通过人家的组件实现的
+      this.data.saveTitle.forEach((item, index) => {
+        //console.log(event)
+        let elTop = event.detail.scrollTop;
+        if (elTop >= item) {
+          console.log(index)
+          this.triggerEvent('ChangeId', {
+            id: index,
+            key: "A"
+          })
+        }
+      })
+    }
+  }
+})
---
+++ pages/list/list.js
@@ -0,0 +1,107 @@
+// pages/list/list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      titleList:[],
+      contentList:[],
+      titleIndex:0,
+      currentKey:"A"
+  },
+
+  changeId(e){
+    //传给子元素  通过子元素调用达到改变颜色的地步
+    //顺便改变key值利用scroll-view这个标签从而进行一些切换
+    let { id } = e.detail;
+    //console.log(key)
+    console.log(id,"===========")
+    this.setData({
+      titleIndex:id
+    })
+  },
+  changekey(e){
+    let {key}=e.detail;
+    this.setData({
+      currentKey: key
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+      wx.request({
+        url: 'https://www.easy-mock.com/mock/5ceb8bac32cfe337f96fe748/example/car#!method=GET&queryParameters=%5B%7B%22enabled%22%3Atrue%2C%22key%22%3A%221%22%2C%22value%22%3A%22%22%7D%5D&body=&headers=%5B%5D',
+        data: '',
+        header: {},
+        method: 'GET',
+        dataType: 'json',
+        responseType: 'text',
+        success:(res)=> {
+          let data = res.data.data.items;
+          let arr=[]
+          data.forEach(item=>{
+            arr.push(item.key)
+          })
+          this.setData({
+            titleList:arr,
+            contentList:data
+          })
+          console.log(this.data.contentList)
+        },
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
