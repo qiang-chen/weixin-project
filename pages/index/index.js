8cee6bc4c01e89ac0690dcc276e449e417d17286
qiang-chen <1715584439@qq.com>
2019-07-18 16:29:39 +0800

    新增主页天气预报功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,56 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    region: ['山东省', '济南市', '历城区'],
+    now:{
+      cond_code:'999',
+      cond_txt:'未知',
+      tmp:0,
+      hum:0,
+      pres:0,
+      vis:0,
+      wind_dir:0,
+      wind_spd:0,
+      wind_sc:0
+    }
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+   
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    this.getWeather()
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  /**
+   * 自定义函数--获取实况天气
+   */
+  getWeather: function() {
+    var that = this
+
+    wx.request({
+      url: 'https://free-api.heweather.com/s6/weather/now',//接口地址
+      data:{
+        location:that.data.region[1],
+        key:'5eedc4b890ed4320b9adb26244f42642'
+      }, //附带参数
+      success:function(res){
+       // console.log(res.data.HeWeather6[0].now)
+       console.log(res)
+        that.setData({
+          now: res.data.HeWeather6[0].now
+        })
+
+      }
     })
+
+  },
+  regionChange(e){
+    let region=e.detail.value
+    this.setData({
+      region
+    },()=>{
+      this.getWeather()
+    })
+    
   }
 })
